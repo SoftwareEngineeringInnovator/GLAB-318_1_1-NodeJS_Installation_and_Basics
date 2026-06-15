e0f151ce84905ef08f1cd395fa1637b9311aa8d8
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-14 22:18:02 -0400

    add custom routes to Node server, like about page and not found page

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World!\n" +
-    "I am a Software Engineering Innovator!\n" +
-    "I am learning Node.js.");
+
+    if (req.url === "/") {
+        res.end(
+            "Home Page\n" +
+            "Hello World!\n" +
+            "I am a Software Engineering Innovator!\n" +
+            "I am learning Node.js."
+        );
+
+    } else if (req.url === "/about") {
+        res.end(
+            "About Page\n" +
+            "This is my first Node.js server.\n" +
+            "I am learning how backend routes work."
+        );
+
+    } else if (req.url === "/contact") {
+        res.end(
+            "Contact Page\n" +
+            "GitHub: SoftwareEngineeringInnovator\n" +
+            "Project: NodeJS Installation and Basics"
+        );
+
+    } else {
+        res.statusCode = 404;
+        res.end("404 Page Not Found");
+    }
 });
 
 server.listen(port, hostname, () => {
